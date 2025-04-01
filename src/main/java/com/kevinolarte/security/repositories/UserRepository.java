aa96e34eb7afa7352baecb5b3a8deab9f04b42b1
kevin <kevinolarte.ko@gmail.com>
2025-04-01 13:10:40 +0200

    Generacion de la primera entity y JWT
    User
    -El user implementara UserDetails para Spring Security muy importante.
    
    UserRepository
    -Creamosu esta clase porque es la encargada con la comunicacion con la
    base de datos.
    
    JwtService
    -La clase encargada de toda la logica en los token JWT. Encagardo de
    generar los tokens, posiblemente la clase mas complicada de todo el
    proyecto

---
+++ src/main/java/com/kevinolarte/security/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kevinolarte.security.repositories;
+
+import com.kevinolarte.security.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+
+
+}
